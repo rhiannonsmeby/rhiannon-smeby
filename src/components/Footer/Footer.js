01391bf51ba1f197c5cc0c1c42960603f5c63ab0
Rhiannon Smeby <smebyrhi@gmail.com>
2021-03-31 16:13:43 -0500

    homepage + colors

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,16 +7,10 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer>
-             <div className="group">
-            <div className="item">
                 <p className="footer-name">&#169; Rhiannon Smeby</p>
-            </div>
-            <div class="item-2">
                 <a href={"mailto:smebyrhi@gmail.com"} target={"_blank"} rel={'noreferrer'}><FontAwesomeIcon icon={faEnvelope}/></a>
                 <a href={"https://github.com/rhiannonsmeby"} target={"_blank"} rel={'noreferrer'}><FontAwesomeIcon icon={faGithub}/></a>
                 <a href={"https://www.linkedin.com/in/rhiannonsmeby/"} target={"_blank"} rel={'noreferrer'}><FontAwesomeIcon icon={faLinkedinIn}/></a>
-            </div>
-        </div>
         </footer>
     );
 }
--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -9,7 +9,7 @@ export default function Homepage() {
             <div className='photos'>
                 <PhotoGrid />
             </div>
-            <div className='introduction'>
+            {/* <div className='introduction'>
                 <h2>Hello,</h2>
                 <h3>I'm Rhiannon</h3>
                 <p>
@@ -24,11 +24,14 @@ export default function Homepage() {
                 <p>
                     Take a look at my website and feel free to <a href={`mailto:smebyrhi@gmail.com`} target={`_blank`}>contact me</a>!
                 </p>
+            </div> */}
             </div>
-            </div>
-            <hr />
             <div className='about-me'>
-                <h2>About me:</h2>
+                <h2>More about me:</h2>
+                <p>
+                    I enjoy programming with JavaScript and I
+                    am passionate about creating web apps that help make life easier and more enjoyable.
+                </p>
                 <p>
                     After graduating college, I began working as a development coordinator
                     at a local nonprofit. I got to design and regularly update the 
@@ -37,12 +40,12 @@ export default function Homepage() {
                     (and fun!) in coding.
                 </p>
                 <p>
-                    Lately, I have been immersed in a full-time Software Engineering  
+                    Recently, I have been immersed in a full-time Software Engineering  
                     certificate program where I learned HTML, CSS, JavaScript, and associated 
                     frontend and backend languages and libraries. 
                 </p>
                 <p>
-                    In addition to programming- I love painting, running, reading, and watching bad reality tv.
+                    In addition to programming- I love painting, spending time outdoors, reading, and watching bad reality tv.
                 </p>
                 <p>
                     Check out my <a href={`https://docs.google.com/document/d/1qi1i3YY0NO8XRGlfLuwDW78kpWSzrpVjXHVJDE9cjCs/edit?usp=sharing`} target={"_blank"} rel={'noreferrer'}>resume</a>, <a href={`https://github.com/rhiannonsmeby`} target={"_blank"} rel={'noreferrer'}>GitHub</a>, or <a href={`https://www.linkedin.com/in/rhiannonsmeby/`} target={"_blank"} rel={'noreferrer'}>LinkedIn</a> to learn more about my qualifications!
